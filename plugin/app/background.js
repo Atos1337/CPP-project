741f342140a3f5498bf5b881d2e593b428cdbcd0
Peter Surkov <petyo333@gmail.com>
2020-02-20 13:21:04 +0300

    Detect downloads

--- plugin/app/background.js
+++ plugin/app/background.js
@@ -1,1 +1,11 @@
-//No background activity for now
+let ids2filename = {};
+
+chrome.downloads.onChanged.addListener(function(delta) {
+  console.log(delta);
+  if (!delta.state ||
+      (delta.state.current != 'complete')) {
+    ids2filename[delta.id] = delta.filename.current;
+    return;
+  }
+  alert(ids2filename[delta.id]);
+});
